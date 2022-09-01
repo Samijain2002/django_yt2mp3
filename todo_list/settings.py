a93e6d95003bb707044a6b930351bc9efc157f02
Samijain2002 <sayyam954@gmail.com>
2022-09-01 07:33:25 +0530

    second update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-sm9_rwrej!6xw(_&i5veu)7b5hr1=#kze45y%nck3@xklwg6qd
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "yvdownloader.herokuapp.com/"]
 
 
 # Application definition
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/base/static"
-# or, eg,
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
